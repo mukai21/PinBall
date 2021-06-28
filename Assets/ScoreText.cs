fcc2729a27b89c9eb8edd1872547226acc5b45c1
mukai21 <82955711+mukai21@users.noreply.github.com>
2021-06-28 19:54:07 +0900

    Initial commit

---
+++ Assets/ScoreText.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class ScoreText : MonoBehaviour
+{
+    private int score = 0;
+    public Text scoreText;
+
+    void Start()
+    {
+        score = 0;
+        SetScore();
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        if(other.gameObject.tag == "LargeStarTag")
+        {
+            score += 10;
+        }
+        SetScore();
+
+
+        if (other.gameObject.tag == "SmallStarTag")
+        {
+            score += 5;
+        }
+        SetScore();
+
+
+        if (other.gameObject.tag == "LargeCloudTag")
+        {
+            score += 20;
+        }
+        SetScore();
+
+        if (other.gameObject.tag == "SmallCloudTag")
+        {
+            score += 15;
+        }
+        SetScore();
+
+    }
+
+    
+    void SetScore()
+    {
+        scoreText.text = string.Format("{0}", score);
+    }
+}
